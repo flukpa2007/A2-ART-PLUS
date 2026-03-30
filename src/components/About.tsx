6444e3405fd42db1f625c22178c48f064c60c8ea
flukpa2007 <flukpa2007@gmail.com>
2026-03-30 11:36:37 +0700

    edit logo icon

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -48,7 +48,7 @@ const About = () => {
           >
             <div className="aspect-square rounded-sm overflow-hidden shadow-2xl border-8 border-zinc-100">
               <img 
-                src="/images/A2-profile.png" 
+                src="/images/logo/logo-a2.png" 
                 alt="Fabrication process" 
                 className="w-full h-full object-cover"
                 referrerPolicy="no-referrer"
