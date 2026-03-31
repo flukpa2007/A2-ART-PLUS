363b02be2f29f0d5470588e5ce872989ac225ad1
flukpa2007 <flukpa2007@gmail.com>
2026-03-31 11:13:54 +0700

    romove plugin sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import Sitemap from 'vite-plugin-sitemap';
 
 
 
@@ -18,12 +17,6 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       react(),
-      Sitemap({ 
-        hostname: 'https://a2artplus.vercel.app', 
-        dynamicRoutes: ['/', '/services', '/portfolio', '/contact'],
-        changefreq: 'weekly', 
-        priority: 1.0,
-      }),
     ],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
