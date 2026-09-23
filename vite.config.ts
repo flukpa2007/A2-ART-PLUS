7dece1257a278d49a546d267b89cb4071b29422b
flukpa2007 <flukpa2007@gmail.com>
2026-09-23 11:24:31 +0700

    Remove unused sitemap routes from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,8 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
-
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
-  
-  const paths = [
-    '/', 
-    '/portfolio', 
-    '/services', 
-    '/contact'
-  ];
 
   return {
     plugins: [
